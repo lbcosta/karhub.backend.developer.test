dca7ad7a30dfb1344d736bc45d4ad046a00fadab
Leonardo Costa <leonardo.costa@guru.com.vc>
2023-04-20 14:34:51 -0300

    Fix: invalid temperature error msg

--- src/api/v1/errors/apierr/apierr.go
+++ src/api/v1/errors/apierr/apierr.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	ErrBeerNotFound       = errors.New("beer of given id not found")
 	ErrBeerAlreadyExists  = errors.New("beer already exists")
-	ErrInvalidTemperature = errors.New("min temperature is higher than max temperature")
+	ErrInvalidTemperature = errors.New("min temperature is higher or equal than max temperature")
 )
